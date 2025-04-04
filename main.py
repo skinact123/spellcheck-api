bd7b45ba34b587b508ac11f34c33b0e19142e07c
skinact123 <arvin.a@skinact.com>
2025-04-04 11:17:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,6 @@ async def spell_check(request: Request):
 
     return {"corrected": corrected}
 
-# Local development or Render deployment
+# Local & production deployment
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
